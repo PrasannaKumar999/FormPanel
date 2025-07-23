864e474b47767b827da5013cdc8d606008707559
Prasannakumar999 <prasanna.kumar54599@gmail.com>
2025-07-23 21:20:33 +0530

    added backend url

--- client/src/app/services/submission.service.ts
+++ client/src/app/services/submission.service.ts
@@ -1,11 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SubmissionService {
-  private API_URL = 'http://localhost:5000/api/submissions';
+  // private API_URL = 'http://localhost:5000/api/submissions';
+  private API_URL = `${environment?.apiUrl}/api/submissions`;
 
   constructor(private http: HttpClient) {}
 
---
+++ client/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://formpanel-dzm5.onrender.com'
+};
